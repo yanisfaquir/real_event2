47d576fda28ee02e29ed2c10b4da802ad4914d1a
yanisfaquir <84600836+yanisfaquir@users.noreply.github.com>
2024-02-24 13:32:50 +0000

    config(pages): page creation

---
+++ src/pages/_app.js
@@ -0,0 +1,13 @@
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div>
+          {/* <Navbar /> */}
+          <div>
+            <Component {...pageProps} />
+          </div>
+    </div>
+  );
+}
+
+export default MyApp;
---
+++ src/pages/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const wishList = () => {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+};
+
+export default wishList;
