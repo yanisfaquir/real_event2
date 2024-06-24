bc26b9f838e3e7c028f44de6772c88c8a977326b
renagold <renatomaourives@ua.pt>
2024-06-24 02:04:53 +0100

    stripe implementation

--- src/pages/shoppingCart.jsx
+++ src/pages/shoppingCart.jsx
@@ -571,6 +571,46 @@ const ShoppingCart = () => {
                 </div>
               </li>
 
+                <GlobalButton
+                  size="medium"
+                  type="primary"
+                  text="Seguinte"
+                  width="40%"
+                  onClick={handleCartSectionClick}
+                />
+              </div>
+            ) : (
+              <GlobalButton
+                size="medium"
+                type="primary"
+                text="CHECKOUT"
+                width="100%"
+                onClick={async () => {
+                  try {
+                    const response = await fetch('http://localhost:3500/shoppingCart/create-checkout-session', {
+                      method: 'POST',
+                      headers: {
+                        'Content-Type': 'application/json'
+                      },
+                      // Se precisar enviar algum dado no corpo da requisição, descomente a linha abaixo
+                      // body: JSON.stringify({ /* dados aqui */ })
+                    });
+                    
+                    if (!response.ok) {
+                      throw new Error('Erro ao criar a sessão de checkout');
+                    }
+
+                    const { url } = await response.json();
+                    window.location.href = url;
+                  } catch (error) {
+                    console.error('Erro ao redirecionar para o checkout:', error);
+                  }
+                }}
+              />
+            )}
+          </div>
+        </div>
+      </section>
               <GlobalButton
                 size="medium"
                 type="primary"
