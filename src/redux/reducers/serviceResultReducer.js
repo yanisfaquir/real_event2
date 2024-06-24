68fb32e6fcc95b0e0c4fa2d384607b27114a7ba5
Patrícia Sousa <116724698+patssousa@users.noreply.github.com>
2024-06-24 04:43:26 +0100

    dados do backend

--- src/redux/reducers/serviceResultReducer.js
+++ src/redux/reducers/serviceResultReducer.js
@@ -4,12 +4,15 @@ import { createSlice } from '@reduxjs/toolkit';
 const serviceResultSlice = createSlice({
   name: 'serviceResult',
   initialState: {
-    serviceType: ['Catering', 'Local', 'Bar'],
-    filters: {
-      preco: '',
-      tipoEspaco: '',
-    },
     selectedService: null,
+    supplierId: '',
+    title: '',
+    description: '',
+    price: '',
+    address: '',
+    num_customers: 0,
+    photo: '',
+    status: '',
   },
   reducers: {
     setFiltroPreco: (state, action) => {
@@ -21,8 +24,45 @@ const serviceResultSlice = createSlice({
     selectService: (state, action) => {
       state.selectedService = action.payload;
     },
+    setSupplierId: (state, action) => {
+      state.supplierId = action.payload;
+    },
+    setTitle: (state, action) => {
+      state.title = action.payload;
+    },
+    setDescription: (state, action) => {
+      state.description = action.payload;
+    },
+    setPrice: (state, action) => {
+      state.price = action.payload;
+    },
+    setAddress: (state, action) => {
+      state.address = action.payload;
+    },
+    setNumCustomers: (state, action) => {
+      state.num_customers = action.payload;
+    },
+    setPhoto: (state, action) => {
+      state.photo = action.payload;
+    },
+    setStatus: (state, action) => {
+      state.status = action.payload;
+    },
   },
 });
 
-export const { setFiltroPreco, setFiltroTipoEspaco, selectService } = serviceResultSlice.actions;
+export const {
+  setFiltroPreco,
+  setFiltroTipoEspaco,
+  selectService,
+  setSupplierId,
+  setTitle,
+  setDescription,
+  setPrice,
+  setAddress,
+  setNumCustomers,
+  setPhoto,
+  setStatus,
+} = serviceResultSlice.actions;
+
 export default serviceResultSlice.reducer;
